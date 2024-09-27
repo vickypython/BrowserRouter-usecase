47d5f12ee22afcbf80e477942db11d69cbd98084
vickypython <vickymlucky6@gmail.com>
2024-09-27 22:34:54 +0300

    optimization of the code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,9 +2,7 @@ import PropTypes from "prop-types"; // Import PropTypes
 import { useState, useMemo, useRef } from "react";
 
 export const Projects = () => {
-  const titleRef = useRef("");
-  const optionRef = useRef("");
-  const [isEditing, setIsEditing] = useState('add');
+  const [isEditing, setIsEditing] = useState("add");
   const [tasks, setTasks] = useState({
     Todo: [
       {
@@ -43,50 +41,87 @@ export const Projects = () => {
   const categories = useMemo(() => {
     return Object.keys(tasks);
   }, [tasks]);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const payload = {
-      id: self.crypto.randomUUID(),
-      title: titleRef.current.value,
-      categorys: optionRef.current.value,
-    };
-    const mode = e.target.getAttribute("mode");
-    if (mode == "add") {
-      addHander(payload);
-    } else {
-      EditHandler(payload);
-    }
-  };
+  console.log("cat:", categories);
+
   const addHander = (payload) => {
+    console.log(payload);
+    //get the tasks which are present in that category
     const templist = [...(tasks[payload.categorys] || [])];
+    console.log("categories:", ...tasks[payload.categorys]);
+
+    console.log("Before templist:", templist);
+    //push the added data(payload) to that category
     templist.push(payload);
+    //update ui to have the current data
     setTasks((prevState) => ({
       ...prevState,
       [payload.categorys]: templist,
     }));
+    console.log("here is the category", [payload.categorys]);
+    console.log("what is here", ([payload.categorys] = templist));
+
+    console.log("this is after:", templist);
   };
+
   const EditHandler = (payload) => {
     const oldTask = isEditing;
     //create new task object
     const newTask = payload;
     //remove what from the old list
     const oldList = tasks[oldTask.categorys].map(
-      (task) => task.id !== oldTask.id
+      (task) => task.id !== newTask.id
     );
     //add it to the new list
-    const newList = [...tasks[newTask.categorys], newTask];
-
+    const newList = [...tasks[newTask.categorys]];
+    //add new task
+    newList.push(newTask);
     setTasks((prev) => ({
       ...prev,
-      [oldTask.categorys]: oldList,
-      [newTask.categorys]: newList,
+      [payload.categorys]: newList,
+      [payload.oldTask.categorys]: oldList,
     }));
-    setIsEditing(null);
+    setIsEditing(undefined);
   };
   return (
     <div style={{ display: "flex", gap: "16px" }}>
       <h1 style={{ opacity: "0.1" }}>Projects</h1>
-      <form>
+      <TaskForm
+        mode={isEditing ? "edit" : "add"}
+        task={EditHandler}
+        addHander={addHander}
+      />
+      {categories.map((category) => (
+        // Pass category name to TaskList component
+        <TaskList key={category} names={category}>
+          {tasks[category].map((task) => (
+            <TaskCard key={task.id} task={task} setIsEditing={setIsEditing} />
+          ))}
+        </TaskList>
+      ))}
+    </div>
+  );
+};
+const TaskForm = ({ addHander, mode = "add", EditHandler, task }) => {
+  const titleRef = useRef("");
+  const optionRef = useRef("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      id: mode === "add" ? self.crypto.randomUUID() : task.id,
+      title: titleRef.current.value,
+      categorys: optionRef.current.value,
+    };
+    const mode = e.target.getAttribute("mode");
+    if (mode == "add") {
+      addHander(payload);
+    } else if (mode == "edit") {
+      EditHandler(payload);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <input
           ref={titleRef}
           type="text"
@@ -108,18 +143,10 @@ export const Projects = () => {
             <option value="complete">complete</option>
           </select>
         </label>
-        <button type="submit" mode="add" onClick={handleSubmit}>
+        <button type="submit" mode="add">
           Add Task
         </button>
       </form>
-      {categories.map((category) => (
-        // Pass category name to TaskList component
-        <TaskList key={category} names={category}>
-          {tasks[category].map((task) => (
-            <TaskCard key={task.id} task={task} />
-          ))}
-        </TaskList>
-      ))}
     </div>
   );
 };
@@ -143,11 +170,13 @@ TaskList.propTypes = {
 };
 
 // TaskCard component will render each task
-const TaskCard = ({ task }) => {
+const TaskCard = ({ task, setIsEditing }) => {
   return (
     <div>
       <p>{task.title}</p>
-      <button type="submit">Edit</button>
+      <button type="submit" mode="edit" onClick={() => setIsEditing(task)}>
+        Edit
+      </button>
     </div>
   );
 };
@@ -157,6 +186,6 @@ TaskCard.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     title: PropTypes.string.isRequired,
+    setIsEditing: PropTypes.func.isRequired,
   }).isRequired,
 };
-
