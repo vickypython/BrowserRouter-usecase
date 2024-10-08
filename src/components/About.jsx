fe08252c36b27fbb2dc6f84ca59d48f99880b1b6
vickypython <vickymlucky6@gmail.com>
2024-10-08 17:32:24 +0300

    updated the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+
 export const About = () => {
+  const [phone, setPhone] = useState("");
+  const [amount, setAmount] = useState("");
+  const [transaction, setTransaction] = useState([
+    {id:1, phone: 254708464263, amount: 30 },
+    {id:2, phone: 254708464263, amount: 100 },
+    { id:3, phone: 254708464263, amount: 100000 },
+  ]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPayment = {
+      id:transaction.length+1,
+      phone: phone,
+      amount: amount,
+    };
+    setTransaction([newPayment,...transaction])
+  };
   return (
     <div>
-      <h1>About</h1>
+      <h1>charity payment list</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="phone">phoneNumber:</label>
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              id="phone"
+              placeholder="Enter phone no"
+            />
+          </div>
+          <div>
+            <label htmlFor="amount">amount:</label>
+            <input
+              type="number"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              id="amount"
+              placeholder="Enter amount"
+            />
+          </div>
+          <button type="submit">pay</button>
+        </form>
+      </div>
+     <div>
+     {transaction.map((trans)=> (
+        <div key={trans.id}>
+          <h2>{trans.phone}</h2>
+          <span>{trans.amount}</span>
+        </div>
+      ))}
+     </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home.jsx";
-import { Projects } from "./components/Projects.jsx";
+// import { Projects } from "./components/Projects.jsx";
+import { About } from "./components/About.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/home" element={<Home/>}/>
-        <Route path="/" element={<Projects />}/>
+        <Route path="/" element={<About />}/>
       </Routes>
     </Router>
   </React.StrictMode>
