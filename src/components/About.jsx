e719af1574567ebc0c72e08812281e86e80a9213
vickypython <vickymlucky6@gmail.com>
2024-10-10 21:28:26 +0300

    api calls updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,52 @@
-import { useState } from "react";
-
+import axios from "axios";
+import { useState,useEffect } from "react";
+const url="https://b53c-43-89-16-94.ngrok-free.app/getTransactions"
 export const About = () => {
   const [phone, setPhone] = useState("");
   const [amount, setAmount] = useState("");
-  const [transaction, setTransaction] = useState([
-    {id:1, phone: 254708464263, amount: 30 },
-    {id:2, phone: 254708464263, amount: 100 },
-    { id:3, phone: 254708464263, amount: 100000 },
-  ]);
-  const handleSubmit = (event) => {
+  const [transaction, setTransaction] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Fetch transactions from the database when the component mounts
+  useEffect(() => {
+    const fetchTransactions = async (url) => {
+      try {
+        const response = await axios.get(url); // Replace with your actual endpoint
+        setTransaction(response.data); // Assuming the response contains the transactions array
+      } catch (err) {
+        setError(err); // Handle error
+        console.error("Error fetching transactions:", err);
+      } finally {
+        setLoading(false); // Stop loading state
+      }
+    };
+
+    fetchTransactions(url);
+  }, [])
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const newPayment = {
-      id:transaction.length+1,
       phone: phone,
       amount: amount,
     };
-    setTransaction([newPayment,...transaction])
+    const response = await axios.post(
+      "https://b53c-41-81-16-94.ngrok-free.app/stkPush",
+      newPayment
+    );
+    console.log("here is the response:", response);
+
+    const dataSaved = response.data;
+    console.log("Data with p&amount:", dataSaved);
+
+    setTransaction((prevState) => [
+      ...prevState,
+      { phone: dataSaved.phone, amount: dataSaved.amount },
+    ]);
   };
+  if(loading)return <div>Loading.....</div>
+  if(error) return <div>Error fetching the Transaction:{error.message}</div>
   return (
     <div>
       <h1>charity payment list</h1>
@@ -45,14 +75,14 @@ export const About = () => {
           <button type="submit">pay</button>
         </form>
       </div>
-     <div>
-     {transaction.map((trans)=> (
-        <div key={trans.id}>
-          <h2>{trans.phone}</h2>
-          <span>{trans.amount}</span>
-        </div>
-      ))}
-     </div>
+      <div>
+        {transaction.map((trans) => (
+          <div key={trans._id}>
+            <h2>{trans.phone}</h2>
+            <span>{trans.amount}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
